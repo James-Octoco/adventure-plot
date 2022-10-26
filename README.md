849f714218767665de76e17705edca81a85cf7c7
James de Villiers <james@octoco.ltd>
2022-10-26 07:16:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# deposition-quality-predictor
---
+++ cdk-lambda/lib/hello-world.ts
@@ -0,0 +1,15 @@
+// This constructs the lambda using NodeJS and the LambdaRestApi construct
+
+import { Construct } from 'constructs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+
+export class HelloWorld extends Construct {
+    constructor(scope: Construct, id: string) {
+        super(scope, id);
+        const helloFunction = new NodejsFunction(this, 'function');
+        new LambdaRestApi(this, 'apigw', {
+            handler: helloFunction,
+        });
+    }
+}
